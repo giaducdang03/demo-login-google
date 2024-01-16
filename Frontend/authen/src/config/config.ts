6e668d211be36676020ec278690f091af7bb0a10
Dương Ton Bao <baodtse171065@fpt.edu.vn>
2024-01-16 14:49:10 +0700

    UPDATE - update headers

--- Frontend/authen/src/config/config.ts
+++ Frontend/authen/src/config/config.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const config = axios.create({
   baseURL: "https://localhost:7235",
   headers: {
-    method: "POST",
     "Content-Type": "application/json",
   },
 });
