b65190b8a87bdd51f6a8b2fe846b0f6270546e36
giaducdang03 <giaducdang@gmail.com>
2024-01-16 12:27:40 +0700

    init project

---
+++ Apis/DemoLoginWithGoogle/Controllers/LoginGoogleController.cs
@@ -0,0 +1,29 @@
+﻿using Google.Apis.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoLoginWithGoogle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginGoogleController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> LoginWithGoogle([FromBody] string credential)
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                // này là demo nên tui để cái key ở đây luôn - ông có thể chỉnh lại thành key của mình nhé
+                Audience = new List<string> { "1054109102936-lk59hetmio3bkttbvvcqmh4gf721jtlj.apps.googleusercontent.com" }
+            };
+            
+            var payload = await GoogleJsonWebSignature.ValidateAsync(credential, settings);
+
+            if (payload == null)
+            {
+                return BadRequest();
+            }
+            return Ok(payload);
+        }
+    }
+}
